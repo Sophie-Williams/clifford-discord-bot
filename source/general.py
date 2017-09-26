a1e3855fca0b617f3dc4e7d0db8c8532645b1ea0
James Will <jwillfiji@gmail.com>
2017-09-26 14:08:55 -0500

    Update general.py
    Updated join command, added leave and serverinfo commands.

--- source/general.py
+++ source/general.py
@@ -58,7 +58,7 @@ class General:
     async def eightball(self, ctx, question: str):
         """Rolls a magic 8-ball to answer any question you have."""
 
-        if question is None:
+        if not question:
             await self.bot.say('{0.message.author.mention}, you did not ask a question.'.format(ctx))
             return
 
@@ -100,28 +100,49 @@ class General:
         result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
         await self.bot.say(result)
 
-    # COMMAND: !get_channel_id
-    @commands.command(pass_context=True)
-    @commands.has_role("Staff")
-    async def get_channel_id(self, ctx):
-        """Lists the ID of the channel the message is sent in."""
-
-        await self.bot.say('Channel ID is {0.id}'.format(ctx.message.channel))
-
     # COMMAND: !join
     @commands.command(pass_context=True)
-    async def join(self, ctx, *, role_name: str):
+    async def join(self, ctx, *, role: str):
         """Allows a user to join a public group."""
 
+        role_name = role.lower()
+
         # List of Allowed Public Roles
-        allowed_roles = ['Europe',
-                         'North America',
-                         'Oceania',
-                         'Overwatch',
-                         'League of Legends',
-                         'Heroes of the Storm',
-                         'Co-op',
-                         'Minna-chan']
+        allowed_roles = ["eu",
+                         "europe",
+                         "na",
+                         "north america",
+                         "oce",
+                         "oceania",
+                         "lol",
+                         "league of legends",
+                         "coop",
+                         "co-op",
+                         "destiny",
+                         "d2",
+                         "destiny 2",
+                         "ow",
+                         "overwatch",
+                         "anime hour",
+                         "anime"]
+
+        roles_dictionary = {"eu": "Europe",
+                            "europe": "Europe",
+                            "na": "North America",
+                            "north america": "North America",
+                            "oce": "Oceania",
+                            "oceania": "Oceania",
+                            "lol": "League of Legends",
+                            "league of legends": "League of Legends",
+                            "coop": "Co-op",
+                            "co-op": "Co-op",
+                            "destiny": "Destiny 2",
+                            "d2": "Destiny 2",
+                            "destiny 2": "Destiny 2",
+                            "ow": "Overwatch",
+                            "overwatch": "Overwatch",
+                            "anime hour": "Anime Hour",
+                            "anime": "Anime Hour"}
 
         if role_name not in allowed_roles:
             await self.bot.say('{0.mention}, you may only join allowed public groups. Check #info for a list of groups.'
@@ -130,16 +151,80 @@ class General:
 
         # Define role, then add role to member.
         try:
-            role = discord.utils.get(ctx.message.server.roles, name=role_name)
+            role = discord.utils.get(ctx.message.server.roles, name=roles_dictionary[role_name])
             await self.bot.add_roles(ctx.message.author, role)
         except Exception as e:
-            await self.bot.send_message(ctx.message.channel, "{0.mention}, there was an error getting the roster for "
-                                                             "you. I'm sorry! : ".format(ctx.message.author) + str(e))
+            await self.bot.send_message(ctx.message.channel, "{0.mention}, there was an error joining the group: "
+                                                             "".format(ctx.message.author) + str(e))
             return
 
         # Success Message
         await self.bot.say('{0.mention}, you have successfully been added to the group **{1}**'
-                           '.'.format(ctx.message.author, role_name))
+                           '.'.format(ctx.message.author, roles_dictionary[role_name]))
+
+    # COMMAND: !leave
+    @commands.command(pass_context=True)
+    async def leave(self, ctx, *, role: str):
+        """Allows a user to leave a public group."""
+
+        role_name = role.lower()
+
+        # List of Allowed Public Roles
+        allowed_roles = ["eu",
+                         "europe",
+                         "na",
+                         "north america",
+                         "oce",
+                         "oceania",
+                         "lol",
+                         "league of legends",
+                         "coop",
+                         "co-op",
+                         "destiny",
+                         "d2",
+                         "destiny 2",
+                         "ow",
+                         "overwatch",
+                         "anime hour",
+                         "anime"]
+
+        roles_dictionary = {"eu": "Europe",
+                            "europe": "Europe",
+                            "na": "North America",
+                            "north america": "North America",
+                            "oce": "Oceania",
+                            "oceania": "Oceania",
+                            "lol": "League of Legends",
+                            "league of legends": "League of Legends",
+                            "coop": "Co-op",
+                            "co-op": "Co-op",
+                            "destiny": "Destiny 2",
+                            "d2": "Destiny 2",
+                            "destiny 2": "Destiny 2",
+                            "ow": "Overwatch",
+                            "overwatch": "Overwatch",
+                            "anime hour": "Anime Hour",
+                            "anime": "Anime Hour"}
+
+        if role_name not in allowed_roles:
+            await self.bot.say(
+                '{0.mention}, you may only join allowed public groups. Check #info for a list of groups.'
+                .format(ctx.message.author))
+            return
+
+        # Define role, then add role to member.
+        try:
+            role = discord.utils.get(ctx.message.server.roles, name=roles_dictionary[role_name])
+            await self.bot.remove_roles(ctx.message.author, role)
+        except Exception as e:
+            await self.bot.send_message(ctx.message.channel,
+                                        "{0.mention}, there was an error leaving the group: "
+                                        "".format(ctx.message.author) + str(e))
+            return
+
+        # Success Message
+        await self.bot.say('{0.mention}, you have successfully left the group **{1}**'
+                           '.'.format(ctx.message.author, roles_dictionary[role_name]))
 
     # COMMAND: !register
     @commands.command(pass_context=True)
@@ -166,6 +251,57 @@ class General:
                                .format(member))
         else:
             await self.bot.say('{0.mention}, you are already registered!'.format(member))
+
+    # COMMAND: !serverinfo
+    @commands.command(pass_context=True)
+    async def serverinfo(self, ctx):
+        """Displays Information about the Server."""
+
+        # Regions Dictionary
+        region_dictionary = {"us-west": "US West",
+                             "us-east": "US East",
+                             "us-central": "US Central",
+                             "eu-west": "EU West",
+                             "eu-central": "EU Central",
+                             "singapore": "Singapore",
+                             "london": "London",
+                             "syndey": "Sydney",
+                             "amsterdam": "Amsterdam",
+                             "frankfurt": "Frankfurt",
+                             "brazil": "Brazil"}
+
+        # Online Status
+        online_statuses = ["online", "idle", "dnd", "do_not_disturb"]
+
+        # GET EMBED INFO
+        server = ctx.message.server
+        server_name = server.name
+        server_region = region_dictionary[str(server.region)]
+        server_date = server.created_at.strftime('%B %d, %Y')
+        server_icon = server.icon_url
+        server_total_members = server.member_count
+        server_id = server.id
+        server_owner = server.owner
+        server_online_members = sum(1 for member in server.members if str(member.status) in online_statuses)
+        server_text_channels = sum(1 for channel in server.channels if str(channel.type) == "text")
+        server_voice_channels = sum(1 for channel in server.channels if str(channel.type) == "voice")
+        server_roles = sorted(server.roles, key=lambda role: role.position, reverse=True)
+        server_roles_list = ", ".join(role.name for role in server_roles if role.hoist is True)
+
+        # Create Embed Table
+        embed = discord.Embed(title=server_name, colour=1203153,
+                              description="Created on " + server_date)
+        embed.set_thumbnail(url=server_icon)
+        embed.set_footer(text="Server ID: " + server_id)
+        embed.add_field(name="Owner", value="{0.mention}".format(server_owner), inline=True)
+        embed.add_field(name="Location", value=server_region, inline=True)
+        embed.add_field(name="Online Members", value=server_online_members, inline=True)
+        embed.add_field(name="Total Members", value=server_total_members, inline=True)
+        embed.add_field(name="Text Channels", value=server_text_channels, inline=True)
+        embed.add_field(name="Voice Channels", value=server_voice_channels, inline=True)
+        embed.add_field(name="Roles", value=server_roles_list, inline=False)
+
+        await self.bot.say(embed=embed)
 
 
 def setup(bot):
